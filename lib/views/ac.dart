1d2c45b822bf4c5b6351d09c6fff7f57dac3efc9
Abdulmalik <abumuhab98@gmail.com>
2021-11-13 15:39:24 +0100

    bug fix

--- lib/views/ac.dart
+++ lib/views/ac.dart
@@ -5,118 +5,121 @@ import 'package:flutter_lagos_challenge/widgets/number_knob_selector.dart';
 class AcView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.7,
-          height: MediaQuery.of(context).size.width * 0.7,
-          child: NumberKnobSelector(
-            trackColor: Colors.green,
-            markingsColor: Colors.blueGrey,
-            fillColor: Colors.blueGrey[900]!,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: MediaQuery.of(context).size.width * 0.7,
+            child: NumberKnobSelector(
+              trackColor: Colors.green,
+              markingsColor: Colors.blueGrey,
+              fillColor: Colors.blueGrey[900]!,
+            ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          height: 50,
-          width: 200,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50), color: Colors.blueGrey),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Weather-shower",
-                style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.blueGrey[900]!),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              SizedBox(
-                height: 20,
-                width: 20,
-                child: Image.asset(
-                  "images/down-arrow.png",
-                  color: Colors.blueGrey[900]!,
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            height: 50,
+            width: 200,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.blueGrey),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Weather-shower",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.blueGrey[900]!),
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: 10,
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: Image.asset(
+                    "images/down-arrow.png",
+                    color: Colors.blueGrey[900]!,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          width: 200,
-          child: Wrap(
-            alignment: WrapAlignment.center,
-            spacing: 10,
-            runSpacing: 10,
-            children: [
-              CustomIconButton(
-                iconData: Icons.ac_unit,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              ),
-              CustomIconButton(
-                iconData: Icons.access_time,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              ),
-              CustomIconButton(
-                iconData: Icons.accessible_outlined,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              ),
-              CustomIconButton(
-                iconData: Icons.account_tree_outlined,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              ),
-              CustomIconButton(
-                iconData: Icons.ad_units_rounded,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              ),
-              CustomIconButton(
-                iconData: Icons.add_chart_sharp,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              ),
-              CustomIconButton(
-                iconData: Icons.adb_rounded,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              ),
-              CustomIconButton(
-                iconData: Icons.account_balance,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              ),
-              CustomIconButton(
-                iconData: Icons.add_alarm,
-                color: Colors.blueGrey[500]!,
-                iconColor: Colors.blueGrey[900]!,
-              )
-            ],
+          SizedBox(
+            height: 15,
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        CustomIconButton(
-          iconData: Icons.power_settings_new,
-          color: Colors.green,
-          iconColor: Colors.black,
-        )
-      ],
+          Container(
+            width: 200,
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              spacing: 10,
+              runSpacing: 10,
+              children: [
+                CustomIconButton(
+                  iconData: Icons.ac_unit,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                ),
+                CustomIconButton(
+                  iconData: Icons.access_time,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                ),
+                CustomIconButton(
+                  iconData: Icons.accessible_outlined,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                ),
+                CustomIconButton(
+                  iconData: Icons.account_tree_outlined,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                ),
+                CustomIconButton(
+                  iconData: Icons.ad_units_rounded,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                ),
+                CustomIconButton(
+                  iconData: Icons.add_chart_sharp,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                ),
+                CustomIconButton(
+                  iconData: Icons.adb_rounded,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                ),
+                CustomIconButton(
+                  iconData: Icons.account_balance,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                ),
+                CustomIconButton(
+                  iconData: Icons.add_alarm,
+                  color: Colors.blueGrey[500]!,
+                  iconColor: Colors.blueGrey[900]!,
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          CustomIconButton(
+            iconData: Icons.power_settings_new,
+            color: Colors.green,
+            iconColor: Colors.black,
+          )
+        ],
+      ),
     );
   }
 }
--- lib/views/lights.dart
+++ lib/views/lights.dart
@@ -5,66 +5,68 @@ import 'package:flutter_lagos_challenge/widgets/knob_selector.dart';
 class LightsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.8,
-          height: MediaQuery.of(context).size.width * 0.8,
-          child: KnobSelector(
-            icon: Image.asset(
-              "images/lamp.png",
-              fit: BoxFit.fill,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.width * 0.8,
+            child: KnobSelector(
+              icon: Image.asset(
+                "images/lamp.png",
+                fit: BoxFit.fill,
+              ),
+              iconContainerColor: Colors.yellow,
+              markingsColor: Colors.blueGrey,
+              fillColor: Colors.blueGrey[900]!,
             ),
-            iconContainerColor: Colors.yellow,
-            markingsColor: Colors.blueGrey,
-            fillColor: Colors.blueGrey[900]!,
           ),
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        Container(
-          height: 50,
-          width: 200,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              color: Colors.blueGrey[500]!),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(
-                Icons.admin_panel_settings_outlined,
-                color: Colors.blueGrey[900]!,
-              ),
-              Icon(
-                Icons.ac_unit,
-                color: Colors.blueGrey[900]!,
-              ),
-              Icon(
-                Icons.attribution_rounded,
-                color: Colors.blueGrey[900]!,
-              ),
-            ],
+          SizedBox(
+            height: 50,
           ),
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        CustomIconButton(
-          iconData: Icons.ac_unit,
-          color: Colors.blueGrey[500]!,
-          iconColor: Colors.blueGrey[900]!,
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        CustomIconButton(
-          iconData: Icons.power_settings_new,
-          color: Colors.yellow,
-          iconColor: Colors.black,
-        )
-      ],
+          Container(
+            height: 50,
+            width: 200,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.blueGrey[500]!),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.admin_panel_settings_outlined,
+                  color: Colors.blueGrey[900]!,
+                ),
+                Icon(
+                  Icons.ac_unit,
+                  color: Colors.blueGrey[900]!,
+                ),
+                Icon(
+                  Icons.attribution_rounded,
+                  color: Colors.blueGrey[900]!,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          CustomIconButton(
+            iconData: Icons.ac_unit,
+            color: Colors.blueGrey[500]!,
+            iconColor: Colors.blueGrey[900]!,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomIconButton(
+            iconData: Icons.power_settings_new,
+            color: Colors.yellow,
+            iconColor: Colors.black,
+          )
+        ],
+      ),
     );
   }
 }
